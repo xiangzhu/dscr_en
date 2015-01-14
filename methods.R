f8c0612e9d0acc00d60e2cf3e8800fe1debfbe6f
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-13 23:59:23 -0600

    add methods: mean, median and winsor

--- methods.R
+++ methods.R
@@ -3,4 +3,8 @@ methods=list()
 #now for each method define a list with its name, function and arguments (if no additional arguments use NULL)
 # like this: 
 #methods[[1]] = list(name="methodname",fn = function,args=NULL)
+
+methods[[1]] = list(name="mean",fn =mean.wrapper,args=NULL)
+methods[[2]] = list(name="median",fn=median.wrapper,args=NULL)
+methods[[3]] = list(name="winsor",fn=winsor.wrapper,args=NULL)
  
