b520189a13cf6cded2152cc8751b0fc79247b2a0
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-14 00:07:00 -0600

    add score functions: squared error, absolute error and root of absolute error

--- score.R
+++ score.R
@@ -2,5 +2,8 @@
 
 score = function(data, output){
 #insert calculations here; return a named list of results
-  return(list())
+  squared_error = (data$meta$truemean-output$meanest)^2 
+  abs_error = abs(data$meta$truemean-output$meanest)
+  rootabs_error = sqrt(abs(data$meta$truemean-output$meanest))
+  return(list(squared_error = squared_error, abs_error= abs_error, rootabs_error=rootabs_error))
 }
