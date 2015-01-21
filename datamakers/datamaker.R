ce1f3e4a029204179c7955245934229b3c81f8c3
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-21 11:20:36 -0600

    update datamaker to reproduce Zou & Hastie (2005)

--- datamakers/datamaker.R
+++ datamakers/datamaker.R
@@ -8,25 +8,58 @@ datamaker = function(seed,args){
   
   #here insert the meat of the function that needs to be defined for each dsc to be done
   #Your function should define the variables meta (a list) and input (a list)
-  nsamp=args$nsamp
-  disttype=args$disttype
 
-  if(disttype=="normal"){
-	input = list(x=rnorm(nsamp,0,1))
-	meta = list(truemean=0)
+  Ntrain = args$Ntrain # training sample size
+  Ntestt = args$Ntestt # test sample size
+  Nvalid = args$Nvalid # validation sample size
+  design = args$design # design matrix type
+  resstd = args$resstd # residual std (sigma) 
+  mybeta = args$mybeta # true coefficient (beta)
+
+  N = Ntrain + Ntestt + Nvalid
+  p = length(mybeta)
+
+  # 1. generate design matrix  
+  if(design=="deccorr"){
+	library(MASS)
+	Sigma = 0.5^abs(outer(1:p, 1:p, FUN = "-"))	
+	X = mvrnorm(n=N, rep(0, p), Sigma)
   }
-  if(disttype=="uniform"){
-	input = list(x=runif(nsamp,-1,1))
-	meta = list(truemean=0)
+  if(design=="eqlcorr"){
+	library(MASS)
+	Sigma = matrix(data=0.5, nrow=p, ncol=p)
+	diag(Sigma) = 1
+	X = mvrnorm(n=N, rep(0, p), Sigma)
   }
-  if(disttype=="Cauchy"){
-	input = list(x=rt(nsamp,df=1))
-	meta = list(truemean=0)
+  if(design=="grouped"){
+	X = matrix(data=0, nrow=N, ncol=p)
+	q = round(p/4)
+	Z = rnorm(3, mean=0, sd=1)
+	E = matrix(data=rnorm(N*p, mean=0, sd=0.01), nrow=N, ncol=p)
+	X[, 1:q] = Z[1] + E[, 1:q]
+	X[, (q+1):(2*q)] = Z[2] + E[, (q+1):(2*q)]
+	X[, (2*q+1):(3*q)] = Z[3] + E[, (2*q+1):(3*q)]
+	X[, (3*q+1):p] = E[, (3*q+1):p]
+	rm(E)   
   }
-  if(disttype=="t4"){
-	input = list(x=rt(nsamp,df=4))
-	meta = list(truemean=0)
-  }  
+
+  # 2. generate response vector
+  epsilon = rnorm(N, mean=0, sd=resstd)  
+  Y = X %*% mybeta + epsilon
+
+  # 3. split sample into train/test/validation set
+  Xtrain = X[1:Ntrain, ]
+  Xtestt = X[(Ntrain+1):(Ntrain+Ntestt), ]
+  Xvalid = X[(Ntrain+Ntestt+1):N, ]
+
+  Ytrain = Y[1:Ntrain]
+  Ytestt = Y[(Ntrain+1):(Ntrain+Ntestt)]
+  Yvalid = Y[(Ntrain+Ntestt+1):N] 
+
+  # 4. aggregate output args
+  meta = list(Xtestt=Xtestt, Ytestt=Ytestt, mybeta=mybeta)
+  input = list(Xtrain=Xtrain, Ytrain=Ytrain, Xvalid=Xvalid, Yvalid=Yvalid)
+ 
   #end of meat of function
   
   data = list(meta=meta,input=input)
