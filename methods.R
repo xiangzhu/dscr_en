7f665003e7c6092db7b42c2bf217bf56ad0f5fb9
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-21 13:41:38 -0600

    add three methods in Zou & Hastie (2005)

--- methods.R
+++ methods.R
@@ -4,7 +4,7 @@ methods=list()
 # like this: 
 #methods[[1]] = list(name="methodname",fn = function,args=NULL)
 
-methods[[1]] = list(name="mean",fn =mean.wrapper,args=NULL)
-methods[[2]] = list(name="median",fn=median.wrapper,args=NULL)
-methods[[3]] = list(name="winsor",fn=winsor.wrapper,args=NULL)
+methods[[1]] = list(name="Lasso", fn=lasso.wrapper,args=NULL)
+methods[[2]] = list(name="ElasticNet", fn=elasticnet.wrapper,args=NULL)
+methods[[3]] = list(name="NaiveElasticNet", fn=naive.elasticnet.wrapper,args=NULL)
  
