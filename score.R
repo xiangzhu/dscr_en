5230703a26e98811468978a56b06095f5a8a1531
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-21 11:33:37 -0600

    add prediction and estimtion MSE as score functions

--- score.R
+++ score.R
@@ -2,8 +2,9 @@
 
 score = function(data, output){
 #insert calculations here; return a named list of results
-  squared_error = (data$meta$truemean-output$meanest)^2 
-  abs_error = abs(data$meta$truemean-output$meanest)
-  rootabs_error = sqrt(abs(data$meta$truemean-output$meanest))
-  return(list(squared_error = squared_error, abs_error= abs_error, rootabs_error=rootabs_error))
+ 
+  prediction_mse = mean( (data$meta$Ytestt - output$predict)^2 )
+  estimation_mse = mean( (data$meta$mybeta - output$coefest)^2 )
+
+  return(list(prediction_mse = prediction_mse, estimation_mse = estimation_mse))
 }
