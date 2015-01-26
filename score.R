74712cff39b939dadf6d07e3bcbb0b6501cb626d
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-26 10:54:37 -0600

    update prediction mse

--- score.R
+++ score.R
@@ -2,10 +2,10 @@
 
 score = function(data, output){
 #insert calculations here; return a named list of results
- 
-  #prediction_mse = mean( (data$meta$Ytestt - output$predict)^2 )
-  prediction_mse = mean( (data$meta$Xtestt %*% data$meta$mybeta - data$meta$Xtestt %*% output$coefest)^2 )
+  
+  prediction = output$predict(data$meta$Xtestt)$fit
+  prediction_mse = mean( (data$meta$Ytestt - prediction)^2 ) - (data$meta$resstd)^2
+  
   estimation_mse = mean( (data$meta$mybeta - output$coefest)^2 )
 
   return(list(prediction_mse = prediction_mse, estimation_mse = estimation_mse))
-}
