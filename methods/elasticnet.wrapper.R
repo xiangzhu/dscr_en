e75c4e2b29bebcd1e6576dd414da35c22c0b3cc0
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-26 12:17:11 -0600

    update EN wrapper: output prediction function; correct the step of choosing tunning parameter

--- methods/elasticnet.wrapper.R
+++ methods/elasticnet.wrapper.R
@@ -9,24 +9,50 @@ elasticnet.wrapper = function(input,args){
   Yvalid = input$Yvalid
   Xtrain = input$Xtrain
   Ytrain = input$Ytrain
-  Xtestt = input$Xtestt
-	
-  # 1. select the tuning parameter using the validation set
+	Mytune = args$Mytune
+  Myiter = args$Myiter
+  
+  # decide tuning parameter
   lambda_list = c(0, 0.01, 0.1, 1, 10, 100)
   num_list = length(lambda_list)
-  cvmin_list = rep(0, num_list)
-  for (j in 1:num_list){
-	cvobj = cv.enet(Xvalid, Yvalid, K=10, lambda_list[j], s=seq(0,1,length=100), mode="fraction", plot.it=FALSE, se=FALSE)
-	cvmin_list[j] = min(cvobj$cv)
+  valid_mse = matrix(0, nrow=num_list, ncol=Myiter)
+  
+  if(is.null(Mytune)){
+    # the method used in Zou and Hastie (2005)
+    for(i in 1:num_list){
+      testobj = enet(Xtrain, Ytrain, lambda_list[i])
+      for(j in 1:Myiter){
+        tuning = 1+(j-1)*0.05
+        Yvalid_fitted = predict(testobj, Xvalid, s=tuning, type="fit", mode="step")$fit
+        valid_mse[i,j] = mean((Yvalid_fitted-Yvalid)^2)
+      }
+    }
+    lambda = lambda_list[which(valid_mse == min(valid_mse), arr.ind = TRUE)[1]]
+    optimS = (which(valid_mse == min(valid_mse), arr.ind = TRUE)[2]-1)*0.05+1
+  } else{
+    # K-fold CV
+    cvmin_list = rep(0, num_list)
+    ostep_list = rep(0, num_list)
+    for (j in 1:num_list){
+      Xmerge = rbind(Xvalid, Xtrain)
+      Ymerge = rbind(Yvalid, Ytrain)
+      cvobj = cv.enet(Xmerge, Ymerge, K=Mytune, lambda_list[j], s=seq(1,1+(Myiter-1)*0.05,length=Myiter), mode="step", plot.it=FALSE, se=FALSE)
+      cvmin_list[j] = min(cvobj$cv)
+      ostep_list[j] = (which.min(cvobj$cv)-1)*0.05+1
+    }
+    lambda = lambda_list[which.min(cvmin_list)] 
+    optimS = ostep_list[which.min(cvmin_list)]
   }
-  lambda = lambda_list[which.min(cvmin_list)]	
-
-  # 2. fit the model using the training set 
+    
+  # fit the model on training set
   myobj = enet(Xtrain, Ytrain, lambda)
-  CPath = myobj$Cp
-  minCp = which.min(CPath)
-  predict = predict.enet(myobj, Xtestt, s=minCp, type="fit", mode="step", naive=FALSE)$fit
-  coefest = predict.enet(myobj, s=minCp, type="coef", mode="step", naive=FALSE)$coefficients 
+  
+  # output prediction function and point estimate
+  predict <- function(Xnew){
+    predict.enet(myobj, Xnew, s=optimS, type="fit", mode="step", naive=FALSE)
+  }
+  
+  coefest = predict.enet(myobj, s=optimS, type="coef", mode="step", naive=FALSE)$coefficients 
 
   return(list(predict=predict, coefest=coefest))
 }
