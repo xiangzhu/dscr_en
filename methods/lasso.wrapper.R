30bd2381c981941d8cb91e8e562a341f8a442397
xiangzhu <xiangzhu.nku@gmail.com>
2015-01-26 12:32:24 -0600

    delete useless lines

--- methods/lasso.wrapper.R
+++ methods/lasso.wrapper.R
@@ -14,35 +14,23 @@ lasso.wrapper = function(input,args){
   
   # decide tuning parameter
   lambda = 0;
-  #lambda_list = c(0, 0.01, 0.1, 1, 10, 100)
-  #num_list = length(lambda_list)
   valid_mse = matrix(0, nrow=1, ncol=Myiter)
   
   if(is.null(Mytune)){
     # the method used in Zou and Hastie (2005)
-    #for(i in 1:num_list){
       testobj = enet(Xtrain, Ytrain, lambda)
       for(j in 1:Myiter){
         tuning = 1+(j-1)*0.05
         Yvalid_fitted = predict(testobj, Xvalid, s=tuning, type="fit", mode="step")$fit
         valid_mse[j] = mean((Yvalid_fitted-Yvalid)^2)
       }
-    #}
-    #lambda = lambda_list[which(valid_mse == min(valid_mse), arr.ind = TRUE)[1]]
     optimS = (which(valid_mse == min(valid_mse), arr.ind = TRUE)[2]-1)*0.05+1
   } else{
     # K-fold CV
-    #cvmin_list = rep(0, num_list)
-    #ostep_list = rep(0, num_list)
-    #for (j in 1:num_list){
       Xmerge = rbind(Xvalid, Xtrain)
       Ymerge = rbind(Yvalid, Ytrain)
       cvobj = cv.enet(Xmerge, Ymerge, K=Mytune, lambda, s=seq(1,1+(Myiter-1)*0.05,length=Myiter), mode="step", plot.it=FALSE, se=FALSE)
-      #cvmin_list[j] = min(cvobj$cv)
       optimS = (which.min(cvobj$cv)-1)*0.05+1
-    #}
-    #lambda = lambda_list[which.min(cvmin_list)] 
-    #optimS = ostep_list[which.min(cvmin_list)]
   }
     
   # fit the model on training set
